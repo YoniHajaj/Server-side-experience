de328c28f5b53b4e988a2384c4c1bfcfa7dd1bfd
YONI HAJAJ <73844661+h1a2j3a4j5@users.noreply.github.com>
2021-06-08 18:45:31 +0300

    server

---
+++ server.js
@@ -0,0 +1,46 @@
+const fastify = require('fastify')({logger:true});
+const fetch =  require("node-fetch");
+
+
+
+
+fastify.register(require('fastify-cors'), { 
+    origin: true, allowedHeaders: ['Origin', 'X-Requested-With', 'Accept', 'Content-Type', 'Authorization'],
+     methods: ['GET', 'PUT', 'OPTIONS', 'POST', 'DELETE'],
+     });
+
+
+
+
+
+fastify.get('/api/location/search/:location', async (request, reply) => {
+    console.log(request.params.location);
+    const response = await  fetch(`https://www.metaweather.com/api/location/search/?query=${request.params.location}`)
+    const data = await response.json();
+    console.log(data);
+    reply.send(data);
+  })
+
+
+  fastify.get('/api/location/:id', async (request, reply) => {
+    console.log(request.params.id);
+    const response = await  fetch(`https://www.metaweather.com/api/location/${request.params.id}/`);
+    const data = await response.json();
+    console.log(data);
+    reply.send(data);
+  })
+
+const start = async () => {
+  try {
+    await fastify.listen(3000);
+  }
+   catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start();
+
+
+
+       
